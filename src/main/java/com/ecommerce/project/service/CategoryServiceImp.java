f299011d7633ef4d25f5b17e1a21ab7321fab720
Rajesh Kumar <rajesh8507kumar@gmail.com>
2024-08-19 23:59:09 +0530

    Repository layer added, implementing JpaRepository

--- src/main/java/com/ecommerce/project/service/CategoryServiceImp.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImp.java
@@ -1,47 +1,46 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class CategoryServiceImp implements CategoryService {
 
-    private List<Category> categories = new ArrayList<>();
-    private Long nextId = 1l;
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void addCategory(Category category) {
-        category.setCategoryId(nextId++);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) {
-        Category categoryToBeDeleted = categories.stream().filter(category -> category.getCategoryId()
-                    .equals(categoryId)).findFirst().orElseThrow(() -> new
+        Category categoryToBeDeleted = categoryRepository.findById(categoryId).orElseThrow(() -> new
                     ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Category with id %s not found", categoryId)));
 
-        categories.remove(categoryToBeDeleted);
+        categoryRepository.delete(categoryToBeDeleted);
         return "Category deleted successfully with category id: " + categoryId;
     }
 
     @Override
     public String updateCategory(Category categoryToBeUpdated, Long categoryId) {
-        Category updatedCategory = categories.stream().filter(category -> category.getCategoryId()
-                .equals(categoryId)).findFirst().orElseThrow(() -> new
+        Category updatedCategory = categoryRepository.findById(categoryId).orElseThrow(() -> new
                 ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Category with id %s not found", categoryId)));
 
         updatedCategory.setCategoryName(categoryToBeUpdated.getCategoryName());
+        categoryRepository.save(updatedCategory);
         return "Category updated successfully with category id: " + categoryId;
     }
 }
